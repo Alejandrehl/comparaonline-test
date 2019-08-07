4efabaf246ffa814aa05038e22828b56eef6c24d
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-07 17:50:15 -0400

    Refactor test

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -1,11 +1,11 @@
 const expect = require("chai").expect;
+
 const CarInsurance = require("../src/classes/CarInsurance");
 const Product = require("../src/classes/Product");
 
-describe("Co Test", () => {
-  it("should foo", () => {
-    const test = new CarInsurance([new Product("fixme", 0, 0)]);
-    test.printResult();
-    expect(test.products[0].name).equal("fixme");
+describe("Co Test", function() {
+  it("should foo", function() {
+    const coTest = new CarInsurance([new Product("foo", 0, 0)]);
+    expect(coTest.products[0].name).equal("foo");
   });
 });
