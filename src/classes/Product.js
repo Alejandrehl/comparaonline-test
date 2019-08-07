0e7304ecb50ee1355d51fd92b0606348556c38bc
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-07 16:58:53 -0400

    Fix in calculate special full coverage

--- src/classes/Product.js
+++ src/classes/Product.js
@@ -21,10 +21,12 @@ class Product {
       if (this.sellIn < 1) inc = 2;
       this.price = Math.max(0, Math.min(50, this.price + inc));
     } else if (this.name == "Special Full Coverage") {
-      if (this.sellIn > 10) this.price = Math.max(0, Math.min(this.price + 1));
+      if (this.sellIn > 10)
+        this.price = Math.max(0, Math.min(50, this.price + 1));
       if (this.sellIn <= 10 && this.sellIn > 5)
-        this.price = Math.max(0, Math.min(this.price + 2));
-      if (this.sellIn <= 5) this.price = Math.max(0, Math.min(this.price + 3));
+        this.price = Math.max(0, Math.min(50, this.price + 2));
+      if (this.sellIn <= 5)
+        this.price = Math.max(0, Math.min(50, this.price + 3));
       if (this.sellIn < 1) this.price = 0;
     } else {
       let deg = 1;
