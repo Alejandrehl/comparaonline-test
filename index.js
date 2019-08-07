dc411e455f281ce212ba39bf04caf54f6b717b60
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-07 14:08:04 -0400

    Classes & for loop refactor

--- index.js
+++ index.js
@@ -14,13 +14,4 @@ const productsAtDayZero = [
 ];
 
 const carInsurance = new CarInsurance(productsAtDayZero);
-const productPrinter = function(product) {
-  console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
-};
-
-for (let i = 1; i <= 30; i += 1) {
-  console.log(`Day ${i}`);
-  console.log("name, sellIn, price");
-  carInsurance.updatePrice().forEach(productPrinter);
-  console.log("");
-}
+carInsurance.printResult();
--- src/classes/Product.js
+++ src/classes/Product.js
@@ -3,6 +3,20 @@ class Product {
     this.name = name;
     this.sellIn = sellIn;
     this.price = price;
+    this.updateValues = function() {
+      this.updatePrice();
+      this.updateSellIn();
+    };
+  }
+
+  updatePrice() {
+    let deg = 1;
+    if (this.sellIn < 1) deg = 2;
+    this.price = Math.max(0, Math.min(50, this.price - deg));
+  }
+
+  updateSellIn() {
+    this.sellIn = this.sellIn - 1;
   }
 }
 
