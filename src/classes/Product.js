f8af73498f67b73befe1b84820a4b7f6e406e299
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-07 16:44:12 -0400

    Full coverage & mega coverage done

--- src/classes/Product.js
+++ src/classes/Product.js
@@ -4,15 +4,27 @@ class Product {
     this.sellIn = sellIn;
     this.price = price;
     this.updateValues = function() {
-      this.updatePrice();
-      this.updateSellIn();
+      this.updateProduct();
     };
   }
 
+  updateProduct() {
+    if (this.name !== "Mega Coverage") {
+      this.updatePrice();
+      this.updateSellIn();
+    }
+  }
+
   updatePrice() {
-    let deg = 1;
-    if (this.sellIn < 1) deg = 2;
-    this.price = Math.max(0, Math.min(50, this.price - deg));
+    if (this.name !== "Full Coverage") {
+      let deg = 1;
+      if (this.sellIn < 1) deg = 2;
+      this.price = Math.max(0, Math.min(50, this.price - deg));
+    } else {
+      let inc = 1;
+      if (this.sellIn < 1) inc = 2;
+      this.price = Math.max(0, Math.min(50, this.price + inc));
+    }
   }
 
   updateSellIn() {
