6e184ef9762c98a8f05aa2d7da50234c83aafcc4
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-07 16:54:46 -0400

    Special full coverage advance

--- src/classes/Product.js
+++ src/classes/Product.js
@@ -16,14 +16,20 @@ class Product {
   }
 
   updatePrice() {
-    if (this.name !== "Full Coverage") {
-      let deg = 1;
-      if (this.sellIn < 1) deg = 2;
-      this.price = Math.max(0, Math.min(50, this.price - deg));
-    } else {
+    if (this.name == "Full Coverage") {
       let inc = 1;
       if (this.sellIn < 1) inc = 2;
       this.price = Math.max(0, Math.min(50, this.price + inc));
+    } else if (this.name == "Special Full Coverage") {
+      if (this.sellIn > 10) this.price = Math.max(0, Math.min(this.price + 1));
+      if (this.sellIn <= 10 && this.sellIn > 5)
+        this.price = Math.max(0, Math.min(this.price + 2));
+      if (this.sellIn <= 5) this.price = Math.max(0, Math.min(this.price + 3));
+      if (this.sellIn < 1) this.price = 0;
+    } else {
+      let deg = 1;
+      if (this.sellIn < 1) deg = 2;
+      this.price = Math.max(0, Math.min(50, this.price - deg));
     }
   }
 
